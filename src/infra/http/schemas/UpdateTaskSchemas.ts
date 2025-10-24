dd98727b5e5bd8b9d1fcbf8a5ef449293c6842ed
Vanorton <vanortton@gmail.com>
2025-10-24 10:53:16 -0300

    infra: remove unsed field at update schema

--- src/infra/http/schemas/UpdateTaskSchemas.ts
+++ src/infra/http/schemas/UpdateTaskSchemas.ts
@@ -2,7 +2,6 @@ import Type from 'typebox'
 import { StatusLiterals } from './CreateTaskSchemas'
 
 export const UpdateTaskSchema = Type.Object({
-    id: Type.String(),
     status: Type.Optional(Type.Union(StatusLiterals)),
     description: Type.Optional(Type.String({ min: 3 })),
 })
